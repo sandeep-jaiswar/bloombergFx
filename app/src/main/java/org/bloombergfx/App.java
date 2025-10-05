5d22ef86a5c4cc5ba42c32a8b75933e5819da097
Sandeep Jaiswar <jaiswarsandeep119@gmail.com>
2025-10-05 19:15:41 +0530

    feat: Update application title to 'BloombergFX' and add application icon

--- app/src/main/java/org/bloombergfx/App.java
+++ app/src/main/java/org/bloombergfx/App.java
@@ -7,17 +7,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        var logoStream = getClass().getResourceAsStream("images/logo.png");
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        if (logoStream != null) {
+            Image icon = new Image(logoStream);
+            stage.getIcons().add(icon);
+        }
 
-        stage.setTitle("JavaFX and Gradle");
+        stage.setTitle("BloombergFX");
         stage.setScene(scene);
         stage.show();
     }
